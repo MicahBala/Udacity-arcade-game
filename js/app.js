4ae05b66d8404dc1314f2506addde8f115e99933
Micah Yunana Bala <micahkulien@gmail.com>
2018-09-02 21:30:01 +0100

    Create Player class and instatntiate player object

--- js/app.js
+++ js/app.js
@@ -24,8 +24,17 @@ class Enemy {
 }
 
 // Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+class Player {
+  constructor() {}
+
+  // This class requires an update(), render() and
+  // a handleInput() method.
+  update() {}
+
+  render() {}
+
+  handleInput() {}
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
@@ -37,6 +46,7 @@ for (let i = 0; i < 5; i++) {
 }
 
 // Place the player object in a variable called player
+player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
